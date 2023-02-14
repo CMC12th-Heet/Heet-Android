38e0135f7fdc5adc96e4c04dae6c85bd44e5aa4e
lee989898 <wnsrud1352@gmail.com>
2023-02-14 18:05:24 +0900

    [add] #5 MainActivity 패키지 위치 변경 및 HeetApp 함수 추가

---
+++ app/src/main/java/org/heet/MainActivity.kt
@@ -0,0 +1,41 @@
+package org.heet
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import org.heet.core.navigation.HeetNavigation
+import org.heet.ui.theme.HeetTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HeetApp()
+        }
+    }
+}
+
+@Composable
+fun HeetApp() {
+    HeetTheme {
+        Surface(
+            color = MaterialTheme.colors.background,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                HeetNavigation()
+            }
+        }
+    }
+}
