06afc9d6add731d87e17c7b9d66ab64fc9858987
lee989898 <wnsrud1352@gmail.com>
2023-02-16 13:33:54 +0900

    [feat] #11 SmallRoundButton, FlatInputField 구현

--- app/src/main/java/org/heet/components/Components.kt
+++ app/src/main/java/org/heet/components/Components.kt
@@ -1,10 +1,8 @@
 package org.heet.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
@@ -17,6 +15,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -102,4 +101,72 @@ fun BigRoundButton(
             color = Color.White
         )
     }
+}
+
+@Composable
+fun SmallRoundButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    text: String
+) {
+    Button(
+        modifier = modifier
+            .height(38.dp),
+        onClick = onClick,
+        shape = RoundedCornerShape(22.dp),
+        border = BorderStroke(1.dp, Red200),
+        colors = ButtonDefaults.buttonColors(Color.White)
+    ) {
+        Text(
+            text = text,
+            fontFamily = pretendardFamily,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Normal,
+            color = Red200
+        )
+    }
+}
+
+@Composable
+fun FlatInputField(
+    modifier: Modifier = Modifier,
+    valueState: MutableState<String>,
+    placeholder: String = "",
+    enabled: Boolean,
+    isSingleLine: Boolean,
+    keyboardType: KeyboardType = KeyboardType.Text,
+    imeAction: ImeAction = ImeAction.Done,
+    onAction: KeyboardActions = KeyboardActions.Default
+) {
+    BasicTextField(
+        modifier = modifier,
+        value = valueState.value,
+        onValueChange = {
+            valueState.value = it
+        },
+        textStyle = TextStyle.Default.copy(
+            color = Grey200,
+            fontSize = 15.sp,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.Normal
+        ),
+        decorationBox = { innerTextField ->
+            Box {
+                if (valueState.value.isEmpty()) {
+                    Text(
+                        text = placeholder,
+                        fontFamily = pretendardFamily,
+                        fontSize = 15.sp,
+                        fontWeight = FontWeight.Normal,
+                        color = Grey200
+                    )
+                }
+                innerTextField()
+            }
+        },
+        singleLine = isSingleLine,
+        enabled = enabled,
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType, imeAction = imeAction),
+        keyboardActions = onAction
+    )
 }
