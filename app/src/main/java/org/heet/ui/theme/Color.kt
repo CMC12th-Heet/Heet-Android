fbf7bf845c4c547d94d61c7ec076c1b07beba2c4
lee989898 <wnsrud1352@gmail.com>
2023-02-16 13:32:46 +0900

    [add] #11 Grey700, Red100, Red400, Red500 색상 추가

--- app/src/main/java/org/heet/ui/theme/Color.kt
+++ app/src/main/java/org/heet/ui/theme/Color.kt
@@ -4,7 +4,11 @@ import androidx.compose.ui.graphics.Color
 
 val Grey200 = Color(0xFFACACAC)
 val Grey500 = Color(0xFF6C6C6C)
+val Grey700 = Color(0xFF656565)
+val Red100 = Color(0xFFFFC9C9)
 val Red200 = Color(0xFFFF5555)
+val Red400 = Color(0xFFFF7878)
+val Red500 = Color(0xFFFF0202)
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
