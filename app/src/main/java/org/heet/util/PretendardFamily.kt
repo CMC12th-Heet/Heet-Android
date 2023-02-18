9875a81a4e1337b65736010b1f29c98c8d3adecb
lee989898 <wnsrud1352@gmail.com>
2023-02-18 16:25:00 +0900

    [add] #13 PretendardFamily에 폰트 추가

--- app/src/main/java/org/heet/util/PretendardFamily.kt
+++ app/src/main/java/org/heet/util/PretendardFamily.kt
@@ -6,9 +6,13 @@ import androidx.compose.ui.text.font.FontWeight
 import org.heet.R
 
 val pretendardFamily = FontFamily(
+    Font(R.font.pretendard_thin, FontWeight.Thin),
+    Font(R.font.pretendard_extralight, FontWeight.ExtraLight),
     Font(R.font.pretendard_light, FontWeight.Light),
     Font(R.font.pretendard_regular, FontWeight.Normal),
     Font(R.font.pretendard_medium, FontWeight.Medium),
+    Font(R.font.pretendard_semibold, FontWeight.SemiBold),
     Font(R.font.pretendard_bold, FontWeight.Bold),
-    Font(R.font.pretendard_extrabold, FontWeight.ExtraBold)
+    Font(R.font.pretendard_extrabold, FontWeight.ExtraBold),
+    Font(R.font.pretendard_black, FontWeight.Black)
 )
