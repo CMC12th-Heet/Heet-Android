af085bb618138a8e8d306d8405aaabeb5f1d5a1c
lee989898 <wnsrud1352@gmail.com>
2023-02-21 21:35:09 +0900

    [ui] #24 로그인 화면 디자인 변경 된 점 수정

--- app/src/main/java/org/heet/presentation/login/LoginScreen.kt
+++ app/src/main/java/org/heet/presentation/login/LoginScreen.kt
@@ -10,7 +10,9 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -22,118 +24,131 @@ import org.heet.components.RoundInputField
 import org.heet.core.navigation.HeetScreens
 import org.heet.ui.theme.Grey600
 import org.heet.ui.theme.Red200
+import org.heet.ui.theme.Red400
 import org.heet.util.pretendardFamily
 
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun LoginScreen(navController: NavController) {
-    val loginIdOrEmail = remember {
-        mutableStateOf("")
-    }
-    val loginPassword = remember {
-        mutableStateOf("")
-    }
-    val isSubscribed = remember {
-        mutableStateOf(true)
-    }
-    val loginIdOrEmailValidState = remember(loginIdOrEmail.value) {
-        loginIdOrEmail.value.trim().isNotEmpty()
-    }
-    val loginPasswordValidState = remember(loginPassword.value) {
-        loginIdOrEmail.value.trim().isNotEmpty()
-    }
+    val loginStateHolder = remember { LoginStateHolder() }
     val keyboardController = LocalSoftwareKeyboardController.current
-    Box(modifier = Modifier.fillMaxSize().padding(horizontal = 20.dp)) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_red_text_logo),
-            contentDescription = null,
-            modifier = Modifier
-                .align(Alignment.TopCenter)
-                .padding(top = 50.dp)
-        )
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(horizontal = 20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        HeetLogo()
         Column(
-            modifier = Modifier
-                .align(Alignment.TopCenter)
-                .padding(top = 239.dp)
+            modifier = Modifier.padding(top = 113.dp)
         ) {
-            RoundInputField(
-                valueState = loginIdOrEmail,
-                placeholder = "아이디 또는 이메일 입력",
-                enabled = true,
-                isSingleLine = true,
-                onAction = KeyboardActions {
-                    if (!loginIdOrEmailValidState) return@KeyboardActions
-                    keyboardController?.hide()
-                }
-            )
-            if (!isSubscribed.value) {
-                Text(
-                    text = "*가입되지 않은 이메일입니다.",
-                    modifier = Modifier.padding(top = 6.dp, start = 19.dp, bottom = 14.dp),
-                    fontFamily = pretendardFamily,
-                    fontSize = 12.sp,
-                    fontWeight = FontWeight.ExtraBold,
-                    color = Red200
+            LoginField(loginStateHolder.idOrEmail, "아이디 또는 이메일 입력", keyboardController)
+            if (!loginStateHolder.isRegistered.value) {
+                RegisterInfo(
+                    "*가입되지 않은 이메일입니다.",
+                    Modifier.padding(top = 6.dp, start = 19.dp, bottom = 14.dp)
                 )
             } else {
-                Spacer(modifier = Modifier.height(30.dp))
+                EmptyText(Modifier.padding(top = 20.dp))
             }
-            RoundInputField(
-                valueState = loginPassword,
-                placeholder = "비밀번호 입력",
-                enabled = true,
-                isSingleLine = true,
-                onAction = KeyboardActions {
-                    if (!loginPasswordValidState) return@KeyboardActions
-                    keyboardController?.hide()
-                }
-            )
-            if (!isSubscribed.value) {
-                Text(
+            LoginField(loginStateHolder.pwd, "비밀번호 입력", keyboardController)
+            if (!loginStateHolder.isRegistered.value) {
+                RegisterInfo(
                     text = "*가입되지 않은 비밀번호입니다.",
-                    modifier = Modifier.padding(top = 6.dp, start = 19.dp),
-                    fontFamily = pretendardFamily,
-                    fontSize = 12.sp,
-                    fontWeight = FontWeight.ExtraBold,
-                    color = Red200
+                    modifier = Modifier.padding(top = 6.dp, start = 19.dp, bottom = 34.dp)
                 )
             } else {
-                Spacer(modifier = Modifier.height(30.dp))
-            }
-            BigRoundButton(
-                onClick = { navController.navigate(HeetScreens.HomeScreen.name) },
-                text = "로그인",
-                modifier = Modifier.padding(top = 34.dp)
-            )
-            Row(
-                modifier = Modifier
-                    .padding(top = 14.dp)
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Text(
-                    text = "비밀번호 찾기",
-                    fontSize = 13.sp,
-                    fontWeight = FontWeight.Normal,
-                    color = Grey600,
-                    modifier = Modifier.clickable { navController.navigate(HeetScreens.FindPasswordScreen.name) }
-                )
-                Divider(
-                    Modifier
-                        .padding(horizontal = 29.dp)
-                        .height(16.5.dp)
-                        .width(2.dp),
-                    color = Red200
-                )
-                Text(
-                    text = "회원가입 하기",
-                    fontSize = 13.sp,
-                    fontWeight = FontWeight.Normal,
-                    color = Grey600,
-                    modifier = Modifier.clickable { navController.navigate(HeetScreens.JoinCertificationScreen.name) }
-                )
+                EmptyText(Modifier.padding(top = 40.dp))
             }
         }
+        LoginButton(navController)
+        Row(
+            modifier = Modifier
+                .padding(top = 12.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Auth("비밀번호 찾기") { navController.navigate(HeetScreens.FindPasswordScreen.name) }
+            Divider(
+                Modifier
+                    .padding(horizontal = 29.dp)
+                    .height(16.5.dp)
+                    .width(2.dp),
+                color = Red400
+            )
+            Auth("회원가입 하기") { navController.navigate(HeetScreens.JoinCertificationScreen.name) }
+        }
     }
+}
+
+@Composable
+private fun HeetLogo() {
+    Image(
+        painter = painterResource(id = R.drawable.ic_red_text_logo),
+        contentDescription = "heet_logo",
+        modifier = Modifier
+            .padding(top = 50.dp)
+    )
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+private fun LoginField(
+    loginStateHolder: MutableState<String>,
+    placeholder: String,
+    keyboardController: SoftwareKeyboardController?
+) {
+    RoundInputField(
+        valueState = loginStateHolder,
+        placeholder = placeholder,
+        enabled = true,
+        isSingleLine = true,
+        onAction = KeyboardActions {
+            if (loginStateHolder.value.trim().isEmpty()) return@KeyboardActions
+            keyboardController?.hide()
+        }
+    )
+}
+
+@Composable
+private fun RegisterInfo(text: String, modifier: Modifier) {
+    Text(
+        text = text,
+        modifier = modifier,
+        color = Red200,
+        fontSize = 12.sp,
+        fontWeight = FontWeight.ExtraBold,
+        fontFamily = pretendardFamily
+    )
+}
+
+@Composable
+private fun EmptyText(modifier: Modifier) {
+    Text(
+        text = "",
+        modifier = modifier,
+        color = Color.White,
+        fontSize = 12.sp,
+        fontWeight = FontWeight.ExtraBold,
+        fontFamily = pretendardFamily
+    )
+}
+
+@Composable
+private fun LoginButton(navController: NavController) {
+    BigRoundButton(
+        onClick = { navController.navigate(HeetScreens.HomeScreen.name) },
+        text = "로그인"
+    )
+}
+
+@Composable
+private fun Auth(text: String, navigate: () -> Unit) {
+    Text(
+        text = text,
+        modifier = Modifier.clickable { navigate() },
+        color = Grey600,
+        fontSize = 13.sp,
+        fontWeight = FontWeight.Normal
+    )
 }
