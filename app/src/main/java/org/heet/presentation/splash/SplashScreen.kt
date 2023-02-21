782b793018456109fd23280a15d8361e79e5c99e
lee989898 <wnsrud1352@gmail.com>
2023-02-21 22:08:57 +0900

    [ui] #27 스플래쉬 화면 디자인 변경 된 점 수정

--- app/src/main/java/org/heet/presentation/splash/SplashScreen.kt
+++ app/src/main/java/org/heet/presentation/splash/SplashScreen.kt
@@ -1,45 +1,34 @@
 package org.heet.presentation.splash
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import kotlinx.coroutines.delay
 import org.heet.R
 import org.heet.core.navigation.HeetScreens
-import org.heet.ui.theme.Red200
 
 @Composable
 fun SplashScreen(navController: NavController) {
-    Surface(color = Red200, modifier = Modifier.fillMaxSize()) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         LaunchedEffect(key1 = true, block = {
-            delay(2000)
+            delay(1500)
             navController.navigate(HeetScreens.LoginScreen.name)
         })
 
-        Box {
-            Image(
-                painter = painterResource(id = R.drawable.ic_image_logo),
-                contentDescription = null,
-                modifier = Modifier
-                    .align(Alignment.TopEnd)
-                    .padding(top = 188.dp)
-            )
-            Image(
-                painter = painterResource(id = R.drawable.ic_white_text_logo),
-                contentDescription = null,
-                modifier = Modifier
-                    .align(Alignment.BottomCenter)
-                    .padding(bottom = 35.dp)
-            )
-        }
+        Image(
+            painter = painterResource(id = R.drawable.ic_image_logo),
+            contentDescription = "heet_logo"
+        )
     }
 }
