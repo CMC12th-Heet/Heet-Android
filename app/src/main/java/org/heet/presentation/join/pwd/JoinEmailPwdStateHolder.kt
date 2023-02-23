b95f3b7cae6c0eef50028fd8f4892712f903d100
lee989898 <wnsrud1352@gmail.com>
2023-02-23 15:02:28 +0900

    [feat] #36 JoinEmailPwdStateHolder 구현

---
+++ app/src/main/java/org/heet/presentation/join/pwd/JoinEmailPwdStateHolder.kt
@@ -0,0 +1,27 @@
+package org.heet.presentation.join.pwd
+
+import androidx.compose.runtime.mutableStateOf
+
+class JoinEmailPwdStateHolder {
+
+    var pwd = mutableStateOf("")
+        private set
+    var secondPwd = mutableStateOf("")
+        private set
+    var isNumber = mutableStateOf(false)
+        private set
+    var isAlphabet = mutableStateOf(false)
+        private set
+    var isSpecialChar = mutableStateOf(false)
+        private set
+    var isValidateLength = mutableStateOf(false)
+        private set
+    var isValidatePwd = mutableStateOf(false)
+        private set
+    var checkPwd = mutableStateOf(false)
+        private set
+    var isHide = mutableStateOf(true)
+        private set
+    var isSame = mutableStateOf(false)
+        private set
+}
