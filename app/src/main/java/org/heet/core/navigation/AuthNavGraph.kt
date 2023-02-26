b3046d37bde336078f57ec190508f70ade7196e5
lee989898 <wnsrud1352@gmail.com>
2023-02-26 23:36:14 +0900

    [feat] #51 AuthNavGraph 구현

---
+++ app/src/main/java/org/heet/core/navigation/AuthNavGraph.kt
@@ -0,0 +1,21 @@
+package org.heet.core.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import org.heet.core.navigation.navscreen.AuthScreen
+import org.heet.presentation.login.LoginScreen
+
+fun NavGraphBuilder.authNavGraph(navController: NavHostController) {
+    navigation(
+        route = Graph.AUTHENTICATION,
+        startDestination = AuthScreen.Login.route
+    ) {
+        composable(route = AuthScreen.Login.route) {
+            LoginScreen(navController = navController)
+        }
+        signupNavGraph(navController = navController)
+        forgotNavGraph(navController = navController)
+    }
+}
