681c90eabac04606e32612c5f21cbd100a0dfc31
lee989898 <wnsrud1352@gmail.com>
2023-02-26 23:37:44 +0900

    [feat] #51 SignUpNavGraph 구현

---
+++ app/src/main/java/org/heet/core/navigation/SignUpNavGraph.kt
@@ -0,0 +1,39 @@
+package org.heet.core.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import org.heet.core.navigation.navscreen.SignUpScreen
+import org.heet.presentation.signup.email.SignUpEmailScreen
+import org.heet.presentation.signup.id.SignUpIdScreen
+import org.heet.presentation.signup.pwd.SignUpPwd
+import org.heet.presentation.signup.residence.SignUpResidenceScreen
+import org.heet.presentation.signup.welcome.SignUpResidenceWelcomeScreen
+import org.heet.presentation.signup.welcome.SignUpWelcomeScreen
+
+fun NavGraphBuilder.signupNavGraph(navController: NavHostController) {
+    navigation(
+        route = Graph.SIGNUP,
+        startDestination = SignUpScreen.SignUpEmail.route
+    ) {
+        composable(route = SignUpScreen.SignUpEmail.route) {
+            SignUpEmailScreen(navController = navController)
+        }
+        composable(route = SignUpScreen.SignUpPwd.route) {
+            SignUpPwd(navController = navController)
+        }
+        composable(route = SignUpScreen.SignUpId.route) {
+            SignUpIdScreen(navController = navController)
+        }
+        composable(route = SignUpScreen.SignUpWelcome.route) {
+            SignUpWelcomeScreen(navController = navController)
+        }
+        composable(route = SignUpScreen.SignUpResidence.route) {
+            SignUpResidenceScreen(navController = navController)
+        }
+        composable(route = SignUpScreen.SignUpResidenceWelcome.route) {
+            SignUpResidenceWelcomeScreen(navController = navController)
+        }
+    }
+}
