8a52572a97321820799b6d82f836d3b59361c58d
lee989898 <wnsrud1352@gmail.com>
2023-02-27 15:52:10 +0900

    [feat] #51 HomeScreen 구현

--- app/src/main/java/org/heet/presentation/home/HomeScreen.kt
+++ app/src/main/java/org/heet/presentation/home/HomeScreen.kt
@@ -1,10 +1,108 @@
 package org.heet.presentation.home
 
-import androidx.compose.material.Text
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.navigation.NavController
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavDestination
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import org.heet.core.navigation.HomeNavGraph
+import org.heet.core.navigation.navscreen.BottomBarScreen
+import org.heet.ui.theme.Grey1700
+import org.heet.ui.theme.Red400
+import org.heet.util.pretendardFamily
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun HomeScreen(navController: NavHostController = rememberNavController()) {
+    Scaffold(
+        bottomBar = { BottomBar(navController = navController) }
+    ) {
+        HomeNavGraph(navController = navController)
+    }
+}
 
 @Composable
-fun HomeScreen(navController: NavController) {
-    Text(text = "Home")
+fun BottomBar(navController: NavHostController) {
+    val screens = listOf(
+        BottomBarScreen.Hometown,
+        BottomBarScreen.FOLLOWING,
+        BottomBarScreen.MyPage
+    )
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
+
+    val bottomBarDestination = screens.any { it.route == currentDestination?.route }
+    if (bottomBarDestination) {
+        BottomNavigation(backgroundColor = Color.White) {
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(start = 41.dp, end = 38.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                screens.forEach { screen ->
+                    AddItem(
+                        screen = screen,
+                        currentDestination = currentDestination,
+                        navController = navController
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AddItem(
+    screen: BottomBarScreen,
+    currentDestination: NavDestination?,
+    navController: NavHostController
+) {
+    val selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true
+    val background = Color.White
+    val contentColor = if (selected) Red400 else Grey1700
+
+    Box(
+        modifier = Modifier
+            .background(background)
+            .clickable(onClick = {
+                navController.navigate(screen.route) {
+                    popUpTo(navController.graph.findStartDestination().id)
+                    launchSingleTop = true
+                }
+            })
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxHeight(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                painter = painterResource(id = if (selected) screen.icon_focused else screen.icon),
+                contentDescription = "icon",
+                tint = contentColor
+            )
+            Spacer(modifier = Modifier.height(3.dp))
+            Text(
+                text = screen.title,
+                color = contentColor,
+                fontSize = 10.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = pretendardFamily
+            )
+        }
+    }
 }
