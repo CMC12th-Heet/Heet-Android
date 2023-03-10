3d84dc1ef3b7315ca6ac74c05fd2d825c3cf0b11
lee989898 <wnsrud1352@gmail.com>
2023-03-10 13:48:03 +0900

    [feat] #96 이메일 인증 로직 구현

--- app/src/main/java/org/heet/presentation/signup/email/SignUpEmailViewModel.kt
+++ app/src/main/java/org/heet/presentation/signup/email/SignUpEmailViewModel.kt
@@ -2,21 +2,43 @@ package org.heet.presentation.signup.email
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import org.heet.data.model.request.RequestPostEmail
+import org.heet.domain.repository.SignUpRepository
+import timber.log.Timber
 import java.text.SimpleDateFormat
 import java.util.*
+import javax.inject.Inject
 
-class SignUpEmailViewModel : ViewModel() {
+@HiltViewModel
+class SignUpEmailViewModel @Inject constructor(private val signUpRepository: SignUpRepository) :
+    ViewModel() {
 
     private var timerCount = 300000
     private val _timer = MutableStateFlow("05:00")
     val timer = _timer.asStateFlow()
 
+    private val _sendEmail = MutableStateFlow(false)
+    val sendEmail = _sendEmail.asStateFlow()
+
     private lateinit var job: Job
+
+    fun postEmail(requestPostEmail: RequestPostEmail) {
+        viewModelScope.launch {
+            runCatching {
+                signUpRepository.postEmail(requestPostEmail)
+            }.onSuccess {
+                _sendEmail.value = true
+            }.onFailure {
+                Timber.d(it.message)
+            }
+        }
+    }
 
     fun timerStart() {
         if (::job.isInitialized) job.cancel()
