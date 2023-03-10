f3e13d67924bd9c4240aa14bf3962a8e49c3479c
lee989898 <wnsrud1352@gmail.com>
2023-03-10 13:46:38 +0900

    [add] #96 SeriveModule에 SingUpService 추가

---
+++ app/src/main/java/org/heet/core/di/ServiceModule.kt
@@ -0,0 +1,20 @@
+package org.heet.core.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.heet.data.service.SignUpService
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object ServiceModule {
+
+    @Provides
+    @Singleton
+    fun provideSignupService(retrofit: Retrofit): SignUpService {
+        return retrofit.create(SignUpService::class.java)
+    }
+}
