34dc7d44b433d904bf2621ddb75e1221e3f650ff
lee989898 <wnsrud1352@gmail.com>
2023-03-10 20:22:02 +0900

    [chore] #100 불필요한 변수 제거 및 companion object로 변경

--- app/src/main/java/org/heet/domain/repository/CodeRepository.kt
+++ app/src/main/java/org/heet/domain/repository/CodeRepository.kt
@@ -3,8 +3,6 @@ package org.heet.domain.repository
 import kotlinx.coroutines.flow.Flow
 
 interface CodeRepository {
-
-    var code: Long
     fun getCode(): Flow<Long>
     suspend fun updateCode(code: Long)
     suspend fun deleteCode()
