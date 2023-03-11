3d21509c68fb6e8bf372d1c8b76257cae52c714e
lee989898 <wnsrud1352@gmail.com>
2023-03-11 20:14:08 +0900

    [del] #107 updateDidLogin 및 deleteDidLogin

--- app/src/main/java/org/heet/data/repository/AutoLoginRepositoryImpl.kt
+++ app/src/main/java/org/heet/data/repository/AutoLoginRepositoryImpl.kt
@@ -5,6 +5,7 @@ import androidx.datastore.preferences.core.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import org.heet.domain.repository.AutoLoginRepository
@@ -23,25 +24,11 @@ class AutoLoginRepositoryImpl @Inject constructor(private val dataStore: DataSto
         }
     }
 
-    override fun getDidLogin(): Flow<Boolean> = dataStore.data.map { preferences ->
-        preferences[DID_LOGIN] ?: false
-    }
-
     override fun getAccessToken(): Flow<String> = dataStore.data.map { preferences ->
         preferences[ACCESS_TOKEN] ?: ""
     }
 
-    override suspend fun updateDidLogin() {
-        dataStore.edit { preferences ->
-            preferences[DID_LOGIN] = true
-        }
-    }
-
-    override suspend fun deleteDidLogin() {
-        dataStore.edit { preferences ->
-            preferences[DID_LOGIN] = false
-        }
-    }
+    override suspend fun getToken(): String = dataStore.data.first()[ACCESS_TOKEN] ?: ""
 
     override suspend fun updateAccessToken(token: String) {
         dataStore.edit { preferences ->
