a5d36ba12f7a08d12f5d4cf2e2c33ddbb69510c1
lee989898 <wnsrud1352@gmail.com>
2023-03-11 10:13:08 +0900

    [feat] #110 getId, updateId, deleteId 구현

--- app/src/main/java/org/heet/domain/repository/StoreEmailPwdRepository.kt
+++ app/src/main/java/org/heet/domain/repository/StoreEmailPwdRepository.kt
@@ -4,9 +4,12 @@ interface StoreEmailPwdRepository {
 
     suspend fun getEmail(): String
     suspend fun getPwd(): String
+    suspend fun getId(): String
 
     suspend fun updateEmail(email: String)
     suspend fun updatePwd(pwd: String)
+    suspend fun updateId(id: String)
     suspend fun deleteEmail(email: String)
     suspend fun deletePwd(pwd: String)
+    suspend fun deleteId(id: String)
 }
