ce10d1c260908105d52763294ecde1a26b431395
lee989898 <wnsrud1352@gmail.com>
2023-03-11 13:24:54 +0900

    [feat] #106 도시 한글, 도시 영어 이름 데이터 클래스 구현

---
+++ app/src/main/java/org/heet/data/datasource/LocalHometownDataSource.kt
@@ -0,0 +1,14 @@
+package org.heet.data.datasource
+
+import org.heet.data.local.Hometown
+
+class LocalHometownDataSource {
+
+    fun loadHometowns(): List<Hometown> {
+        return listOf(
+            Hometown("서울", "seoul"),
+            Hometown("경기", "gyeonggi"),
+            Hometown("인천", "incheon")
+        )
+    }
+}
---
+++ app/src/main/java/org/heet/data/local/Hometown.kt
@@ -0,0 +1,6 @@
+package org.heet.data.local
+
+data class Hometown(
+    val cityKr: String,
+    val cityEng: String
+)
