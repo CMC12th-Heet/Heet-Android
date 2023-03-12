47c3f1bf694b9a768232e52003bec22822b76703
lee989898 <wnsrud1352@gmail.com>
2023-03-12 11:46:51 +0900

    [feat] #126 MyPage api 기초 세팅

---
+++ app/src/main/java/org/heet/domain/repository/MyPageRepository.kt
@@ -0,0 +1,10 @@
+package org.heet.domain.repository
+
+import org.heet.data.model.request.RequestModifyMyPage
+import org.heet.data.model.response.*
+
+interface MyPageRepository {
+
+    suspend fun getMyPage(): ResponseGetMyPage
+    suspend fun modifyMyPAge(requestModifyMyPage: RequestModifyMyPage): ResponseModifyMyPage
+}
