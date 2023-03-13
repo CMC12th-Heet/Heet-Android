41560d65bb3de0f6b45f86674a4c6b5f5dc16844
lee989898 <wnsrud1352@gmail.com>
2023-03-13 17:39:09 +0900

    [chore] #121 ResponsePostStore Int로 변경

--- app/src/main/java/org/heet/data/service/StoreService.kt
+++ app/src/main/java/org/heet/data/service/StoreService.kt
@@ -2,7 +2,7 @@ package org.heet.data.service
 
 import org.heet.data.model.request.RequestPostStore
 import org.heet.data.model.response.ResponseGetStore
-import org.heet.data.model.response.ResponsePostStore
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Query
@@ -10,7 +10,7 @@ import retrofit2.http.Query
 interface StoreService {
 
     @POST("/store")
-    suspend fun postStore(requestPostStore: RequestPostStore): ResponsePostStore
+    suspend fun postStore(@Body requestPostStore: RequestPostStore): Int
 
     @GET("/store")
     suspend fun getStore(@Query("keyword") keyword: String): List<ResponseGetStore>
