18d6b1df1da67c84b6de2a65a3c47464472283c6
lee989898 <wnsrud1352@gmail.com>
2023-03-13 13:14:39 +0900

    [feat] #120 Verify 로직 구현

---
+++ app/src/main/java/org/heet/presentation/home/hometown/verify/VerifyViewModel.kt
@@ -0,0 +1,41 @@
+package org.heet.presentation.home.hometown.verify
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import org.heet.domain.repository.PostRepository
+import org.heet.domain.repository.VerifyRepository
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class VerifyViewModel @Inject constructor(
+    private val postRepository: PostRepository,
+    private val verifyRepository: VerifyRepository
+) : ViewModel() {
+
+    private val _verify = MutableStateFlow(false)
+    val verify = _verify.asStateFlow()
+
+    private fun updateVerify(verify: Boolean) {
+        viewModelScope.launch {
+            verifyRepository.updateIsVerify(verify)
+        }
+    }
+
+    fun postVerify(x: String, y: String) {
+        viewModelScope.launch {
+            runCatching {
+                postRepository.postVerify(x, y)
+            }.onSuccess {
+                updateVerify(it.message != "fail")
+                _verify.value = it.message != "fail"
+            }.onFailure {
+                Timber.d(it)
+            }
+        }
+    }
+}
