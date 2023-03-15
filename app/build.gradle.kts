01bb989584f50893690321f6b1ca79fb974537d3
lee989898 <wnsrud1352@gmail.com>
2023-03-15 18:24:08 +0900

    [chore] 버전코드 변경 1 -> 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "org.heet"
         minSdk = 23
         targetSdk = 33
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
