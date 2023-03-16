033c236a8a5defdbfee5b79be7d5d6aef3ca4ed7
lee989898 <wnsrud1352@gmail.com>
2023-03-16 18:12:59 +0900

    [chore] #145 version code 3 -> 4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "org.heet"
         minSdk = 23
         targetSdk = 33
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
