4fb54253356ae9c7e254b216bc0f55f2cf7c67df
lee989898 <wnsrud1352@gmail.com>
2023-03-16 12:30:20 +0900

    [chore] #143 Topbar 관련 컴포넌트 묶기

---
+++ app/src/main/java/org/heet/components/TopBarComponents.kt
@@ -0,0 +1,59 @@
+package org.heet.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import org.heet.R
+import org.heet.ui.theme.Red500
+import org.heet.util.pretendardFamily
+
+@Composable
+fun Back(modifier: Modifier = Modifier, back: () -> Unit) {
+    Image(
+        painter = painterResource(id = R.drawable.ic_back_black_30),
+        contentDescription = "back",
+        modifier = modifier.clickable { back() }
+    )
+}
+
+@Composable
+fun Title(text: String) {
+    Text(
+        text = text,
+        color = Color.Black,
+        fontSize = 17.sp,
+        fontWeight = FontWeight.Normal,
+        fontFamily = pretendardFamily
+    )
+}
+
+@Composable
+fun Next(
+    onClick: () -> Unit
+) {
+    Text(
+        text = "다음",
+        modifier = Modifier.clickable {
+            onClick()
+        },
+        color = Red500,
+        fontSize = 17.sp,
+        fontWeight = FontWeight.Black
+    )
+}
+
+@Composable
+fun EmptyText() {
+    Text(
+        text = "빈곳",
+        color = Color.White,
+        fontSize = 17.sp,
+        fontWeight = FontWeight.Black
+    )
+}
