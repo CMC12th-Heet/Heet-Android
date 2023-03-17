b40d754a6e464b78f876c2983b0ecb3f077b8aef
lee989898 <wnsrud1352@gmail.com>
2023-03-17 13:56:40 +0900

    [ui] #147 회원가입 비밀번호 화면 디자인 수정

--- app/src/main/java/org/heet/presentation/signup/pwd/SignUpPwdScreen.kt
+++ app/src/main/java/org/heet/presentation/signup/pwd/SignUpPwdScreen.kt
@@ -28,167 +28,144 @@ fun SignUpPwd(
     signupPwdViewModel: SingUpPwdViewModel = hiltViewModel()
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
-    val pwd = remember {
-        mutableStateOf("")
-    }
-    val secondPwd = remember {
-        mutableStateOf("")
-    }
-    val isNumber = remember {
-        mutableStateOf(false)
-    }
-    val isAlphabet = remember {
-        mutableStateOf(false)
-    }
-    val isSpecialChar = remember {
-        mutableStateOf(false)
-    }
-    val isValidateLength = remember {
-        mutableStateOf(false)
-    }
-    val isValidatePwd = remember {
-        mutableStateOf(false)
-    }
-    val checkPwd = remember {
-        mutableStateOf(false)
-    }
-    val isHide = remember {
-        mutableStateOf(true)
-    }
-    val isSame = remember {
-        mutableStateOf(false)
-    }
+    val pwd = remember { mutableStateOf("") }
+    val secondPwd = remember { mutableStateOf("") }
+    val isNumber = remember { mutableStateOf(false) }
+    val isAlphabet = remember { mutableStateOf(false) }
+    val isSpecialChar = remember { mutableStateOf(false) }
+    val isValidateLength = remember { mutableStateOf(false) }
+    val isValidatePwd = remember { mutableStateOf(false) }
+    val checkPwd = remember { mutableStateOf(false) }
+    val isHide = remember { mutableStateOf(true) }
+    val isSame = remember { mutableStateOf(false) }
 
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(horizontal = 20.dp)
-            .padding(top = 18.dp)
+            .padding(top = 14.dp)
     ) {
-        Column {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Back { navController.popBackStack() }
-                Title("회원 가입")
-                if (isSame.value) {
-                    Next { navController.navigate(SignUpScreen.SignUpId.route) }
-                } else {
-                    EmptyText()
-                }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Back { navController.popBackStack() }
+            Title("회원 가입")
+            if (isSame.value) {
+                Next { navController.navigate(SignUpScreen.SignUpId.route) }
+            } else {
+                EmptyText()
             }
+        }
+        Spacer(modifier = Modifier.height(61.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "이메일",
+                color = Grey300,
+                fontSize = 17.sp,
+                fontWeight = FontWeight.Normal,
+                fontFamily = pretendardFamily
+            )
+            Text(
+                text = signupPwdViewModel.getEmail(),
+                color = White250,
+                fontSize = 15.sp,
+                fontWeight = FontWeight.Normal,
+                fontFamily = pretendardFamily
+            )
+        }
+        Column(modifier = Modifier.padding(top = 36.dp)) {
+            Text(
+                text = "비밀번호를 설정하세요*",
+                color = Grey900,
+                fontSize = 17.sp,
+                fontWeight = FontWeight.Normal,
+                fontFamily = pretendardFamily
+            )
+            Spacer(modifier = Modifier.height(18.dp))
             Row(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 61.dp),
+                    .fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Text(
-                    text = "이메일",
-                    color = Grey300,
-                    fontSize = 17.sp,
-                    fontWeight = FontWeight.Normal,
-                    fontFamily = pretendardFamily
-                )
-                Text(
-                    text = signupPwdViewModel.getEmail(),
-                    color = White250,
-                    fontSize = 15.sp,
-                    fontWeight = FontWeight.Normal,
-                    fontFamily = pretendardFamily
-                )
-            }
-            Column(modifier = Modifier.padding(top = 36.dp)) {
-                Text(
-                    text = "새 비밀번호를 입력하세요*",
-                    fontFamily = pretendardFamily,
-                    fontSize = 17.sp,
-                    fontWeight = FontWeight.Normal,
-                    color = Grey900
-                )
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 18.dp)
-                ) {
-                    PwdField(
-                        pwd = pwd,
-                        pwdValidState = pwd.value.trim().isNotEmpty(),
-                        isHide = isHide,
-                        keyboardController = keyboardController,
-                        isNumber = isNumber,
-                        isAlphabet = isAlphabet,
-                        isSpecialChar = isSpecialChar,
-                        isValidateLength = isValidateLength,
-                        isValidatePwd = isValidatePwd,
-                        checkPwd = checkPwd
-                    )
-                    Hide(
-                        isHide = isHide.value,
-                        modifier = Modifier.align(Alignment.CenterEnd)
-                    ) {
-                        isHide.value = !isHide.value
-                    }
-                }
-                Divider(
-                    Modifier
-                        .padding(top = 12.dp)
-                        .fillMaxWidth()
-                        .height(3.dp),
-                    color = White350
-                )
-            }
-            if (!checkPwd.value) {
-                Column(modifier = Modifier.padding(12.dp)) {
-                    ValidateText("숫자", isNumber.value)
-                    ValidateText("영문", isAlphabet.value)
-                    ValidateText("특수문자", isSpecialChar.value)
-                    ValidateText("8자 이상", isValidateLength.value)
-                }
-            }
-
-            Column(modifier = Modifier.padding(top = 18.dp)) {
-                Text(
-                    text = "비밀번호 재확인*",
-                    fontFamily = pretendardFamily,
-                    fontSize = 17.sp,
-                    fontWeight = FontWeight.Normal,
-                    color = Grey900
-                )
-                SecondPwdField(
+                PwdField(
                     pwd = pwd,
-                    secondPwd = secondPwd,
-                    secondPwdValidState = secondPwd.value.trim()
-                        .isNotEmpty(),
+                    pwdValidState = pwd.value.trim().isNotEmpty(),
                     isHide = isHide,
-                    isSame = isSame,
-                    keyboardController = keyboardController
+                    keyboardController = keyboardController,
+                    isNumber = isNumber,
+                    isAlphabet = isAlphabet,
+                    isSpecialChar = isSpecialChar,
+                    isValidateLength = isValidateLength,
+                    isValidatePwd = isValidatePwd,
+                    checkPwd = checkPwd
                 )
-                Divider(
-                    Modifier
-                        .padding(top = 12.dp)
-                        .fillMaxWidth()
-                        .height(3.dp),
-                    color = White350
-                )
-                if (isSame.value) {
-                    signupPwdViewModel.updatePwd(pwd.value)
-                    Row(modifier = Modifier.padding(top = 15.dp, start = 12.dp)) {
-                        Image(
-                            painter = painterResource(id = R.drawable.ic_check_black_20),
-                            contentDescription = null,
-                            modifier = Modifier.padding(end = 7.dp)
-                        )
-                        Text(
-                            text = "일치합니다.",
-                            color = White750,
-                            fontSize = 13.sp,
-                            fontWeight = FontWeight.Normal,
-                            fontFamily = pretendardFamily
-                        )
-                    }
+                Hide(isHide = isHide.value) {
+                    isHide.value = !isHide.value
+                }
+            }
+            Spacer(modifier = Modifier.height(12.dp))
+            Divider(
+                Modifier
+                    .fillMaxWidth()
+                    .height(3.dp),
+                color = White350
+            )
+        }
+        Spacer(modifier = Modifier.height(12.dp))
+        if (!checkPwd.value) {
+            Column(modifier = Modifier.padding(start = 12.dp)) {
+                ValidateText("숫자", isNumber.value)
+                ValidateText("영문", isAlphabet.value)
+                ValidateText("특수문자", isSpecialChar.value)
+                ValidateText("8자 이상", isValidateLength.value)
+            }
+        }
+        Spacer(modifier = Modifier.height(22.dp))
+        Column {
+            Text(
+                text = "비밀번호 재확인*",
+                color = Grey900,
+                fontSize = 17.sp,
+                fontWeight = FontWeight.Normal,
+                fontFamily = pretendardFamily
+            )
+            SecondPwdField(
+                pwd = pwd,
+                secondPwd = secondPwd,
+                secondPwdValidState = secondPwd.value.trim()
+                    .isNotEmpty(),
+                isHide = isHide,
+                isSame = isSame,
+                keyboardController = keyboardController
+            )
+            Spacer(modifier = Modifier.height(12.dp))
+            Divider(
+                Modifier
+                    .fillMaxWidth()
+                    .height(3.dp),
+                color = White350
+            )
+            if (isSame.value) {
+                signupPwdViewModel.updatePwd(pwd.value)
+                Row(modifier = Modifier.padding(top = 15.dp, start = 12.dp)) {
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_check_black_20),
+                        contentDescription = null,
+                        modifier = Modifier.padding(end = 7.dp)
+                    )
+                    Text(
+                        text = "일치합니다.",
+                        color = White750,
+                        fontSize = 13.sp,
+                        fontWeight = FontWeight.Normal,
+                        fontFamily = pretendardFamily
+                    )
                 }
             }
         }
