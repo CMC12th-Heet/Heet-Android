2e6041d5bd3ddea4c7f56781159c845c3771ff8a
lee989898 <wnsrud1352@gmail.com>
2023-03-17 17:39:21 +0900

    [add] #149 MyPageService 회원탈퇴 추가

--- app/src/main/java/org/heet/data/service/MyPageService.kt
+++ app/src/main/java/org/heet/data/service/MyPageService.kt
@@ -3,6 +3,8 @@ package org.heet.data.service
 import org.heet.data.model.request.RequestModifyMyPage
 import org.heet.data.model.response.ResponseGetMyPage
 import org.heet.data.model.response.ResponseModifyMyPage
+import org.heet.data.model.response.ResponseWithdraw
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 
@@ -13,4 +15,7 @@ interface MyPageService {
 
     @POST("/user/my-page")
     suspend fun modifyMyPage(requestModifyMyPage: RequestModifyMyPage): ResponseModifyMyPage
+
+    @DELETE("/user")
+    suspend fun withdraw(): ResponseWithdraw
 }
