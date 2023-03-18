f5d766c34d7bf0d861e001d0ecc204321293ee86
lee989898 <wnsrud1352@gmail.com>
2023-03-18 11:26:44 +0900

    [feat] #124 댓글 보기 기능 구현

---
+++ app/src/main/java/org/heet/presentation/home/hometown/comment/CommentViewModel.kt
@@ -0,0 +1,33 @@
+package org.heet.presentation.home.hometown.comment
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import org.heet.data.model.response.ResponseGetComment
+import org.heet.domain.repository.CommentRepository
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class CommentViewModel @Inject constructor(
+    private val commentRepository: CommentRepository
+) : ViewModel() {
+
+    private val _commentList = MutableStateFlow(emptyList<ResponseGetComment>())
+    val commentList = _commentList.asStateFlow()
+
+    fun getComment(postId: String) {
+        viewModelScope.launch {
+            runCatching {
+                commentRepository.getComment(postId)
+            }.onSuccess {
+                _commentList.value = it
+            }.onFailure {
+                Timber.d(it.message)
+            }
+        }
+    }
+}
