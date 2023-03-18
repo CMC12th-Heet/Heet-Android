a60a18c762f3ba1eedacf8593757f60667b7a091
lee989898 <wnsrud1352@gmail.com>
2023-03-18 11:25:23 +0900

    [chore] #124 CommentService Query를 Path로 변경

--- app/src/main/java/org/heet/data/service/CommentService.kt
+++ app/src/main/java/org/heet/data/service/CommentService.kt
@@ -3,27 +3,23 @@ package org.heet.data.service
 import org.heet.data.model.response.ResponseDeleteComment
 import org.heet.data.model.response.ResponseGetComment
 import org.heet.data.model.response.ResponsePostComment
-import retrofit2.http.Body
-import retrofit2.http.DELETE
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface CommentService {
 
     @POST("/comment/{id}")
     suspend fun postComment(
-        @Query("id") id: String,
+        @Path("id") id: String,
         @Body content: String
     ): ResponsePostComment
 
     @GET("/comment/{id}")
     suspend fun getComment(
-        @Query("id") id: String
-    ): ResponseGetComment
+        @Path("id") id: String
+    ): List<ResponseGetComment>
 
     @DELETE("/comment/{id}")
     suspend fun deleteComment(
-        @Query("id") id: String
+        @Path("id") id: String
     ): ResponseDeleteComment
 }
