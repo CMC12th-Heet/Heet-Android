3410a4208ca85e487fb50575b87f987d67150ccc
lee989898 <wnsrud1352@gmail.com>
2023-03-21 09:19:16 +0900

    [feat] #105 LoadTermsDataSource 구현

---
+++ app/src/main/java/org/heet/data/datasource/LoadTermsDataSource.kt
@@ -0,0 +1,31 @@
+package org.heet.data.datasource
+
+import org.heet.data.local.TermItem
+
+class LoadTermsDataSource {
+
+    fun loadTerms(): List<TermItem> {
+        return listOf(
+            TermItem(
+                "[필수] 개인정보 수집 및 이용 동의",
+                "https://heetkr.notion.site/003152352a4e4f05b3607b1a790d3827",
+                false
+            ),
+            TermItem(
+                "[필수] 이용약관 및 위치기반 서비스 이용약관 동의",
+                "https://heetkr.notion.site/HEET-6590ce86b1b54526ad1fead10ae8f9b9",
+                false
+            ),
+            TermItem(
+                "[필수] 만 14세 이상입니다.",
+                "",
+                false
+            ),
+            TermItem(
+                "[선택] 마케팅 활용 및 광고성 정보 수신 동의",
+                "https://heetkr.notion.site/7d1df952675344f58640174795208fd6",
+                false
+            )
+        )
+    }
+}
---
+++ app/src/main/java/org/heet/data/local/TermItem.kt
@@ -0,0 +1,7 @@
+package org.heet.data.local
+
+data class TermItem(
+    val term: String,
+    val url: String,
+    val isSelected: Boolean
+)
