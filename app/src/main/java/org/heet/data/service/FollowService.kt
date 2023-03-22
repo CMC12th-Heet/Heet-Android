7a624c28ce119a0315d34ccbac7db4c3877c9133
lee989898 <wnsrud1352@gmail.com>
2023-03-22 15:09:47 +0900

    [feat] #175 FollowService getFollower, getFollowing 추가

--- app/src/main/java/org/heet/data/service/FollowService.kt
+++ app/src/main/java/org/heet/data/service/FollowService.kt
@@ -1,6 +1,9 @@
 package org.heet.data.service
 
+import org.heet.data.model.response.ResponseGetFollower
+import org.heet.data.model.response.ResponseGetFollowing
 import org.heet.data.model.response.ResponsePostFollow
+import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
 
@@ -8,4 +11,10 @@ interface FollowService {
 
     @POST("/user/follow/{id}")
     suspend fun postFollow(@Path("id") id: String): ResponsePostFollow
+
+    @GET("/user/follower")
+    suspend fun getFollower(): List<ResponseGetFollower>
+
+    @GET("/user/following")
+    suspend fun getFollowing(): List<ResponseGetFollowing>
 }
