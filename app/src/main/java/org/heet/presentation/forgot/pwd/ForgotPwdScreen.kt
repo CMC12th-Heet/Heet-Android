be2e87ed634eb580bc48fdb06552526cd410ea61
lee989898 <wnsrud1352@gmail.com>
2023-03-23 15:42:28 +0900

    [feat] #188 비밀번호 재설정 로직 구현

--- app/src/main/java/org/heet/presentation/forgot/pwd/ForgotPwdScreen.kt
+++ app/src/main/java/org/heet/presentation/forgot/pwd/ForgotPwdScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import org.heet.R
 import org.heet.components.*
@@ -24,143 +25,126 @@ import org.heet.util.pretendardFamily
 @OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun ForgotPwdScreen(
-    navController: NavController
+    navController: NavController,
+    email: String,
+    pwdViewModel: PwdViewModel = hiltViewModel()
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
-    val pwd = remember {
-        mutableStateOf("")
-    }
-    val secondPwd = remember {
-        mutableStateOf("")
-    }
-    val isNumber = remember {
-        mutableStateOf(false)
-    }
-    val isAlphabet = remember {
-        mutableStateOf(false)
-    }
-    val isSpecialChar = remember {
-        mutableStateOf(false)
-    }
-    val isValidateLength = remember {
-        mutableStateOf(false)
-    }
-    val isValidatePwd = remember {
-        mutableStateOf(false)
-    }
-    val checkPwd = remember {
-        mutableStateOf(false)
-    }
-    val isHide = remember {
-        mutableStateOf(true)
-    }
-    val isSame = remember {
-        mutableStateOf(false)
+    val pwd = remember { mutableStateOf("") }
+    val secondPwd = remember { mutableStateOf("") }
+    val isNumber = remember { mutableStateOf(false) }
+    val isAlphabet = remember { mutableStateOf(false) }
+    val isSpecialChar = remember { mutableStateOf(false) }
+    val isValidateLength = remember { mutableStateOf(false) }
+    val isValidatePwd = remember { mutableStateOf(false) }
+    val checkPwd = remember { mutableStateOf(false) }
+    val isHide = remember { mutableStateOf(true) }
+    val isSame = remember { mutableStateOf(false) }
+
+    LaunchedEffect(pwdViewModel.resetSuccess.collectAsState().value) {
+        if (pwdViewModel.resetSuccess.value) {
+            navController.navigate(AuthScreen.Login.route) {
+                popUpTo(0)
+            }
+        }
     }
 
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(horizontal = 20.dp)
-            .padding(top = 18.dp)
+            .padding(top = 14.dp)
     ) {
-        Column {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Back { navController.popBackStack() }
-                Title("비밀번호 재설정")
-                if (isSame.value) {
-                    Finish(
-                        modifier = Modifier.clickable {
-                            navController.navigate(AuthScreen.Login.route) {
-                                popUpTo(0)
-                            }
-                        }
-                    )
-                } else {
-                    EmptyText()
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Back { navController.popBackStack() }
+            Title("비밀번호 재설정")
+            if (isSame.value) {
+                Finish(
+                    modifier = Modifier.clickable {
+                        pwdViewModel.resetEmail(pwd.value, email)
+                    }
+                )
+            } else {
+                EmptyText()
+            }
+        }
+        Spacer(modifier = Modifier.height(158.dp))
+        PretendardDescription("새 비밀번호를 입력하세요*")
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            PwdField(
+                pwd = pwd,
+                pwdValidState = pwd.value.trim().isNotEmpty(),
+                isHide = isHide,
+                keyboardController = keyboardController,
+                isNumber = isNumber,
+                isAlphabet = isAlphabet,
+                isSpecialChar = isSpecialChar,
+                isValidateLength = isValidateLength,
+                isValidatePwd = isValidatePwd,
+                checkPwd = checkPwd
+            )
+            Hide(isHide = isHide.value) {
+                isHide.value = !isHide.value
+            }
+        }
+        Divider(
+            Modifier
+                .fillMaxWidth()
+                .height(3.dp),
+            color = White350
+        )
+        if (!checkPwd.value) {
+            if (pwd.value.isNotEmpty()) {
+                Column(modifier = Modifier.padding(12.dp)) {
+                    ValidateText("숫자", isNumber.value)
+                    ValidateText("영문", isAlphabet.value)
+                    ValidateText("특수문자", isSpecialChar.value)
+                    ValidateText("8자 이상", isValidateLength.value)
                 }
             }
-            Column(modifier = Modifier.padding(top = 153.dp)) {
-                PretendardDescription("새 비밀번호를 입력하세요*")
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 13.dp)
-                ) {
-                    PwdField(
-                        pwd = pwd,
-                        pwdValidState = pwd.value.trim().isNotEmpty(),
-                        isHide = isHide,
-                        keyboardController = keyboardController,
-                        isNumber = isNumber,
-                        isAlphabet = isAlphabet,
-                        isSpecialChar = isSpecialChar,
-                        isValidateLength = isValidateLength,
-                        isValidatePwd = isValidatePwd,
-                        checkPwd = checkPwd
-                    )
-                    Hide(
-                        isHide = isHide.value,
-                        modifier = Modifier.align(Alignment.CenterEnd)
-                    ) {
-                        isHide.value = !isHide.value
-                    }
-                }
+        } else {
+            Spacer(modifier = Modifier.height(18.dp))
+            Column {
+                PretendardDescription("비밀번호 재확인*")
+                SecondPwdField(
+                    pwd = pwd,
+                    secondPwd = secondPwd,
+                    secondPwdValidState = secondPwd.value.trim()
+                        .isNotEmpty(),
+                    isHide = isHide,
+                    isSame = isSame,
+                    keyboardController = keyboardController
+                )
                 Divider(
                     Modifier
-                        .padding(top = 13.dp)
                         .fillMaxWidth()
                         .height(3.dp),
                     color = White350
                 )
-                if (!checkPwd.value) {
-                    if (pwd.value.isNotEmpty()) {
-                        Column(modifier = Modifier.padding(12.dp)) {
-                            ValidateText("숫자", isNumber.value)
-                            ValidateText("영문", isAlphabet.value)
-                            ValidateText("특수문자", isSpecialChar.value)
-                            ValidateText("8자 이상", isValidateLength.value)
-                        }
-                    }
-                } else {
-                    Column(modifier = Modifier.padding(top = 18.dp)) {
-                        PretendardDescription("비밀번호 재확인*")
-                        SecondPwdField(
-                            pwd = pwd,
-                            secondPwd = secondPwd,
-                            secondPwdValidState = secondPwd.value.trim()
-                                .isNotEmpty(),
-                            isHide = isHide,
-                            isSame = isSame,
-                            keyboardController = keyboardController
+                if (isSame.value) {
+                    Row(modifier = Modifier.padding(top = 15.dp, start = 12.dp)) {
+                        Image(
+                            painter = painterResource(id = R.drawable.ic_check_black_20),
+                            contentDescription = null,
+                            modifier = Modifier.padding(end = 7.dp)
                         )
-                        Divider(
-                            Modifier
-                                .padding(top = 13.dp)
-                                .fillMaxWidth()
-                                .height(3.dp),
-                            color = White350
+                        Text(
+                            text = "일치합니다.",
+                            color = White750,
+                            fontSize = 13.sp,
+                            fontWeight = FontWeight.Normal,
+                            fontFamily = pretendardFamily
                         )
-                        if (isSame.value) {
-                            Row(modifier = Modifier.padding(top = 15.dp, start = 12.dp)) {
-                                Image(
-                                    painter = painterResource(id = R.drawable.ic_check_black_20),
-                                    contentDescription = null,
-                                    modifier = Modifier.padding(end = 7.dp)
-                                )
-                                Text(
-                                    text = "일치합니다.",
-                                    color = White750,
-                                    fontSize = 13.sp,
-                                    fontWeight = FontWeight.Normal,
-                                    fontFamily = pretendardFamily
-                                )
-                            }
-                        }
                     }
                 }
             }
---
+++ app/src/main/java/org/heet/presentation/forgot/pwd/PwdViewModel.kt
@@ -0,0 +1,33 @@
+package org.heet.presentation.forgot.pwd
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import org.heet.data.model.request.RequestResetPwd
+import org.heet.domain.repository.ResetRepository
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class PwdViewModel @Inject constructor(
+    private val resetRepository: ResetRepository
+) : ViewModel() {
+
+    private val _resetSuccess = MutableStateFlow(false)
+    val resetSuccess = _resetSuccess.asStateFlow()
+
+    fun resetEmail(password: String, email: String) {
+        viewModelScope.launch {
+            runCatching {
+                resetRepository.resetPwd(RequestResetPwd(password, email))
+            }.onSuccess {
+                _resetSuccess.value = true
+            }.onFailure {
+                Timber.d(it.message)
+            }
+        }
+    }
+}
