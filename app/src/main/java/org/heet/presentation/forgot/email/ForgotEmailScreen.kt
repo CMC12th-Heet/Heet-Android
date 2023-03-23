71059a7d868a0c83b5cc8de7221cf95d32678213
lee989898 <wnsrud1352@gmail.com>
2023-03-23 15:21:34 +0900

    [feat] #188 비밀번호 리셋 이메일 전송 로직 구현

--- app/src/main/java/org/heet/presentation/forgot/email/ForgotEmailScreen.kt
+++ app/src/main/java/org/heet/presentation/forgot/email/ForgotEmailScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
@@ -17,6 +18,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import org.heet.components.*
 import org.heet.core.navigation.navscreen.ForgotScreen
+import org.heet.data.model.request.RequestPostEmail
 import org.heet.ui.theme.*
 import org.heet.util.pretendardFamily
 import java.util.*
@@ -28,146 +30,169 @@ fun ForgotEmailScreen(
     forgotEmailViewModel: ForgotEmailViewModel = hiltViewModel()
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
-    val email = remember {
-        mutableStateOf("")
-    }
-    val code = remember {
-        mutableStateOf("")
-    }
-    val requestEmail = remember {
-        mutableStateOf(false)
-    }
-    val requestCode = remember {
-        mutableStateOf(false)
-    }
-    val verificationTimer = remember {
-        forgotEmailViewModel.timer
-    }
+    val email = remember { mutableStateOf("") }
+    val code = remember { mutableStateOf("") }
+    val requestCode = remember { mutableStateOf(false) }
+    val verificationTimer = forgotEmailViewModel.timer.collectAsState().value
+    val sendEmail = forgotEmailViewModel.sendEmail.collectAsState().value
+    val isCorrectCode = forgotEmailViewModel.isCorrectCode.collectAsState().value
 
-    Box(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(horizontal = 20.dp)
-            .padding(top = 18.dp)
+            .padding(top = 14.dp)
     ) {
-        Column {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Back { navController.popBackStack() }
-                Title("비밀번호 찾기")
-                if (requestCode.value) {
-                    Next { navController.navigate(ForgotScreen.ForgotPwd.route) }
-                    forgotEmailViewModel.timerReset()
-                } else {
-                    EmptyText()
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Back { navController.popBackStack() }
+            Title("비밀번호 찾기")
+            if (isCorrectCode) {
+                Next {
+                    forgotEmailViewModel.deleteCode()
+                    navController.navigate(ForgotScreen.ForgotPwd.route)
+                }
+            } else {
+                EmptyText()
+            }
+        }
+        Spacer(modifier = Modifier.height(157.dp))
+        PretendardDescription("이메일*")
+        Spacer(modifier = Modifier.height(13.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            FlatTextField(
+                modifier = Modifier.width(203.dp),
+                valueState = email,
+                enabled = true,
+                isSingleLine = true,
+                keyboardType = KeyboardType.Email,
+                onAction = KeyboardActions {
+                    if (email.value.trim().isEmpty()) return@KeyboardActions
+                    keyboardController?.hide()
+                }
+            )
+            RequestBtn(sendEmail, "인증 요청") {
+                if (email.value.trim().isNotEmpty()) {
+                    forgotEmailViewModel.postEmail(RequestPostEmail(email = email.value))
                 }
             }
-            Column(modifier = Modifier.padding(top = 153.dp)) {
-                PretendardDescription("이메일*")
+        }
+        Spacer(modifier = Modifier.height(13.dp))
+        Divider(
+            Modifier
+                .fillMaxWidth()
+                .height(3.dp),
+            color = White350
+        )
+        if (sendEmail) {
+            Column {
+                Spacer(modifier = Modifier.height(36.dp))
+                SendCode(
+                    verificationCode = code,
+                    verificationTimer = verificationTimer,
+                    verificationCodeValidState = code.value.trim().isNotEmpty(),
+                    requestCode = requestCode,
+                    isCorrectCode = isCorrectCode,
+                    forgotEmailViewModel = forgotEmailViewModel,
+                    keyboardController = keyboardController
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Divider(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(2.dp),
+                    color = White350
+                )
+                Spacer(modifier = Modifier.height(20.dp))
                 Row(
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 13.dp),
+                        .fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceBetween,
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    FlatTextField(
-                        modifier = Modifier.width(203.dp),
-                        valueState = email,
-                        enabled = true,
-                        isSingleLine = true,
-                        keyboardType = KeyboardType.Email,
-                        onAction = KeyboardActions {
-                            if (email.value.trim().isEmpty()) return@KeyboardActions
-                            keyboardController?.hide()
-                        }
-                    )
-                    RequestBtn(requestEmail, "인증 요청") {
-                        requestEmail.value = true
-                    }
-                }
-                Divider(
-                    Modifier
-                        .padding(top = 13.dp)
-                        .fillMaxWidth()
-                        .height(3.dp),
-                    color = White350
-                )
-                if (requestEmail.value) {
+                    Description("인증번호를 받지 못하셨나요?", modifier = Modifier.padding(start = 8.5.dp))
                     Column(
-                        modifier = Modifier
-                            .padding(top = 36.dp)
+                        modifier = Modifier.width(IntrinsicSize.Max).padding(end = 8.5.dp)
                     ) {
-                        Box(modifier = Modifier.fillMaxWidth()) {
-                            FlatTextField(
-                                modifier = Modifier
-                                    .padding(end = 182.dp)
-                                    .align(Alignment.CenterStart),
-                                valueState = code,
-                                placeholder = "인증코드 입력",
-                                enabled = true,
-                                isSingleLine = true,
-                                keyboardType = KeyboardType.Email,
-                                onAction = KeyboardActions {
-                                    if (code.value.trim()
-                                        .isEmpty()
-                                    ) return@KeyboardActions
-                                    keyboardController?.hide()
-                                }
-                            )
-                            Row(
-                                modifier = Modifier.align(Alignment.CenterEnd),
-                                verticalAlignment = Alignment.CenterVertically
-                            ) {
-                                Text(
-                                    text = verificationTimer.collectAsState().value,
-                                    modifier = Modifier.padding(end = 21.dp),
-                                    color = Red900,
-                                    fontSize = 14.sp,
-                                    fontWeight = FontWeight.Normal,
-                                    fontFamily = pretendardFamily
+                        ReSendBtn(isCorrectCode) {
+                            forgotEmailViewModel.postEmail(
+                                RequestPostEmail(
+                                    email.value
                                 )
-                                RequestBtn(isCheck = requestCode, "인증 요청") {
-                                    if (!requestCode.value) {
-                                        forgotEmailViewModel.timerStart()
-                                    }
-                                    requestCode.value = true
-                                }
-                            }
+                            )
                         }
                         Divider(
                             Modifier
-                                .padding(top = 10.dp)
-                                .fillMaxWidth()
-                                .height(2.dp),
-                            color = White150
+                                .padding(top = 2.dp)
+                                .height(1.dp),
+                            color = Grey700
                         )
-                    }
-                    Row(
-                        modifier = Modifier
-                            .padding(top = 20.dp)
-                            .fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Description("인증번호를 받지 못하셨나요?", modifier = Modifier.padding(start = 8.5.dp))
-                        Column(
-                            modifier = Modifier.width(IntrinsicSize.Max).padding(end = 8.5.dp)
-                        ) {
-                            ReSendBtn(requestCode.value) { forgotEmailViewModel.timerStart() }
-                            Divider(
-                                Modifier
-                                    .padding(top = 2.dp)
-                                    .height(1.dp),
-                                color = Grey700
-                            )
-                        }
                     }
                 }
             }
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+private fun SendCode(
+    verificationCode: MutableState<String>,
+    verificationTimer: String,
+    verificationCodeValidState: Boolean,
+    requestCode: MutableState<Boolean>,
+    isCorrectCode: Boolean,
+    forgotEmailViewModel: ForgotEmailViewModel,
+    keyboardController: SoftwareKeyboardController?
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        FlatTextField(
+            modifier = Modifier
+                .padding(start = 9.dp, end = 117.dp)
+                .align(Alignment.CenterStart),
+            valueState = verificationCode,
+            placeholder = "인증코드 입력",
+            enabled = true,
+            isSingleLine = true,
+            keyboardType = KeyboardType.Email,
+            onAction = KeyboardActions {
+                if (!verificationCodeValidState) return@KeyboardActions
+                keyboardController?.hide()
+            }
+        )
+        Row(
+            modifier = Modifier.align(Alignment.CenterEnd),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            val text = if (isCorrectCode) "인증 완료" else "인증 요청"
+            Text(
+                text = verificationTimer,
+                modifier = Modifier.padding(end = 21.dp),
+                color = Red900,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Normal,
+                fontFamily = pretendardFamily
+            )
+            RedSmallRoundButton22(
+                onClick = {
+                    if (!requestCode.value) {
+                        forgotEmailViewModel.postCode(verificationCode.value)
+                    }
+                },
+                text = text,
+                isCheck = isCorrectCode
+            )
         }
     }
 }
