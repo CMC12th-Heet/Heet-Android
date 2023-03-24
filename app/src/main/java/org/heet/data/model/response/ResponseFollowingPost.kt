6af7b6ae0bd2db52922489d0fb9ebeeba14b0e86
lee989898 <wnsrud1352@gmail.com>
2023-03-24 17:42:44 +0900

    [add] #195 ResponseFollowingPost 데이터 클래스 추가

---
+++ app/src/main/java/org/heet/data/model/response/ResponseFollowingPost.kt
@@ -0,0 +1,47 @@
+package org.heet.data.model.response
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ResponseFollowingPost(
+    val posts: List<Post>,
+) {
+    @Serializable
+    data class Post(
+        val content: String,
+        val created_at: String,
+        val file_url: String,
+        val is_local: Boolean,
+        val mini_title: String,
+        val moving_tip: String?,
+        val ordering_tip: String?,
+        val other_tips: String?,
+        val perfect_day: String?,
+        val post_id: Int,
+        val satisfaction: Int?,
+        val store: Store,
+        val title: String,
+        val together_with: String?,
+        val user: User,
+        val urlList: List<String>,
+    ) {
+        @Serializable
+        data class Store(
+            val address: String,
+            val name: String,
+            val store_id: Int,
+            val url: String,
+        )
+
+        @Serializable
+        data class User(
+            val email: String,
+            val is_verify: Boolean,
+            val password: String,
+            val status: String?,
+            val town: String,
+            val user_id: Int,
+            val username: String,
+        )
+    }
+}
