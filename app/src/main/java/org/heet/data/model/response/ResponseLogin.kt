c9b55ed4add51e6c3cc0bfb611da8f1471373348
lee989898 <wnsrud1352@gmail.com>
2023-03-24 23:26:33 +0900

    [chore] #197 ResponseLogin 타운 변수 추가

--- app/src/main/java/org/heet/data/model/response/ResponseLogin.kt
+++ app/src/main/java/org/heet/data/model/response/ResponseLogin.kt
@@ -6,9 +6,11 @@ import org.heet.domain.entity.LoginInfo
 @Serializable
 data class ResponseLogin(
     val message: String,
-    val token: String
+    val token: String,
+    val town: String,
 ) {
     fun toLoginInfo() = LoginInfo(
-        token
+        token,
+        town,
     )
 }
